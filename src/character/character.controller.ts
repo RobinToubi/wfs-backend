37ea0ebebc2a47beb5a44461b8180d33b889d95d
Robin Gimenez <gimenez.robin11@gmail.com>
2021-04-16 19:52:20 +0200

    Init

---
+++ src/character/character.controller.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import { AbstractController } from '../common/abstract.controller';
+import { AuthenticatedRequest } from '../common/authentication.middleware';
+import { CharacterModel, ICharacter } from './character.model';
+import { characterService } from './character.service';
+
+class CharacterController extends AbstractController<CharacterModel> {
+  protected service = characterService;
+
+  findAll(request: AuthenticatedRequest, response: Response, next: NextFunction) {
+    if (request.query.favorite === 'true') {
+      this.service.findAllFavorites(request.userToken.id)
+        .then(dtos => response.json(dtos))
+        .catch(next);
+    } else {
+      super.findAll(request, response, next);
+    }
+  }
+}
+
+export const characterController = new CharacterController();

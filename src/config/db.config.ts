02e27c99a4952019abec4c5db0af4d07e7c24567
Abel POUILLET <abel.pouillet@ynov.com>
2021-04-18 19:05:51 +0200

    config abel

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,8 +1,8 @@
 import { ConnectionOptions } from "mongoose";
 
 // MONGO URL
-const password = "Ui7p89R9SSEDsIk8";
-const username = "robin";
+const password = "BQGGEfDjkuBRm91v";
+const username = "abel";
 const databaseName = 'webfullstack'
 export const MONGO_URL = "mongodb+srv://" + username + ":" + password + "@cluster0.6jyqv.mongodb.net/" + databaseName + "?retryWrites=true&w=majority";
 

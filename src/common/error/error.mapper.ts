22d5e87b20d88cab2059f3d4ee8a4283301df8e5
agazirce <48948789+agazirce@users.noreply.github.com>
2021-04-22 16:13:16 +0200

    update middleware

--- src/common/error/error.mapper.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorType } from './error.model';
-import { PrimaryKeyError } from './repository-error.model';
-
-export const itemErrorHandler = id => error => {
-  if (error instanceof PrimaryKeyError) {
-    return Promise.reject({ type: ErrorType.resourceIdNotFound, messageParam: id });
-  }
-  return Promise.reject(error);
-};
